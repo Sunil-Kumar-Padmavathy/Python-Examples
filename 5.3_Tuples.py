8fb0cd2390df3375a05679c30c090e1abdbc9508
Sunil Kumar Padmavathy <SunilKumar.Padmavathy@gmail.com>
2018-03-31 23:59:39 -0400

    new examples

---
+++ 5.3_Tuples.py
@@ -0,0 +1,14 @@
+#tuples examples
+print("\ntuples examples")
+
+a= 12,13, 'numbers'
+print(a)
+print("a[0] -->",a[0])
+
+print("nexted tuple\n")
+print("b = a, (54,88,'numbers2')\n")
+b = a, (54,88,'numbers2')
+print("b-->",b)
+
+
+
