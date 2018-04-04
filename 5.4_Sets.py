86538789dc699ae9abb5dd45a0a8345896f9d735
Sunil Kumar Padmavathy <SunilKumar.Padmavathy@gmail.com>
2018-04-04 00:00:27 -0400

    set examples

---
+++ 5.4_Sets.py
@@ -0,0 +1,21 @@
+#5.4 sets examples
+print("sets example--> use set() function or {} to create sets \n")
+numbers={"one","two","three","four","one","two"}
+print("numbers set -->" , numbers,"\n")
+print(numbers,"\n")
+
+print("notice that duplicates are removed\n"
+"Also notice that everytime we execute, \n"
+"the order of the items in the result set change\n")
+
+print("test existence of an item in the set\n")
+print("'one' in numbers")
+print('one' in numbers,"\n")
+
+print("'nothing' in numbers")
+print('nothing' in numbers)
+
+print("find unique letters\n")
+a= set('anaesthesia')
+b= set('pharmaceutical')
+
